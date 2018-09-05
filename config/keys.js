4679cb082b2090ad91480b68c3cf244b6703269b
Mix Master Mike <mikeyamato@gmail.com>
2018-09-04 22:42:29 -0700

    latest commit

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-// to make an object available outside of this file use `module.exports`
-
-module.exports = {
-		mongoURI: 'mongodb://mikeyamato:msy053177@ds227332.mlab.com:27332/udemy-devconnector',
-		secretORKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+// to make an object available outside of this file use `module.exports`
+
+module.exports = {
+	mongoURI: process.env.MONGO_URI,
+	secretORKey: process.env.SECRET_OR_KEY
+};
