f3addc38f46b8b46de8e26ff2af914182773dd50
Mix Master Mike <mikeyamato@gmail.com>
2018-11-22 09:53:31 -0800

    updated redux devtools store.js

--- client/src/store.js
+++ client/src/store.js
@@ -7,14 +7,29 @@ const initialState = {};
 // why an array? 
 const middleware = [thunk];
 
-// `[]` = reducer, `{}` = initial state
+// // `[]` = reducer, `{}` = initial state
+// const store = createStore(
+// 	rootReducer, 
+// 	initialState, 
+// 	compose(
+// 		applyMiddleware(...middleware),  // `...` = spread operator
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	)  
+// ); 
+
+const enhancers = [];
+const isDevelopment = process.env.NODE_ENV === 'development';
+if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+  enhancers.push(window.devToolsExtension());
+}
+
 const store = createStore(
-	rootReducer, 
-	initialState, 
-	compose(
-		applyMiddleware(...middleware),  // `...` = spread operator
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)  
-);  
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    ...enhancers,
+  ),
+);
 
 export default store;
